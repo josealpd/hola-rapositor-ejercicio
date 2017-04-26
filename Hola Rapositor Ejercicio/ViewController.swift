f157a919e14446d2a720719c92797a32591c65fe
Joseal Pantoja <joseal.pantoja@gmail.com>
2017-04-26 11:12:29 -0500

    Initial Commit

---
+++ Hola Rapositor Ejercicio/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hola Rapositor Ejercicio
+//
+//  Created by Joseal Pantoja on 26/04/17.
+//  Copyright © 2017 Joseal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
